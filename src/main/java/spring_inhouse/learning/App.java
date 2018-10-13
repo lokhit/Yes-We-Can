c5c5021ca1f4059da02cbbb108211c8dc23e8c3e
lovish <lovish@hp>
2018-10-13 17:20:43 +0530

    Initial project checkin

---
+++ src/main/java/spring_inhouse/learning/App.java
@@ -0,0 +1,13 @@
+package spring_inhouse.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
